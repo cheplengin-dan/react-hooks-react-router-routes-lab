dd2142f53e31cb82a592d0101c300df51c1ba225
duncan kiptoo <cheplenginduncan@gmail.com>
2023-05-03 14:23:03 +0300

    add class to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,21 +3,21 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-<div>
-<NavLink exact to="/">
-Home
-</NavLink>
-<NavLink exact to="/movies">
-Movies
-</NavLink>
-<NavLink exact to="/directors">
-Directors
-</NavLink>
-<NavLink exact to="/actors">
-Actors
-</NavLink>
-</div>
-);
+    <div className="navbar">
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink exact to="/movies">
+        Movies
+      </NavLink>
+      <NavLink exact to="/directors">
+        Directors
+      </NavLink>
+      <NavLink exact to="/actors">
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
